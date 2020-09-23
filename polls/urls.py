261831857bb81245e4fdd5971980768b4b17852b
zql0809 <2021192371@qq.com>
2020-09-23 23:50:10 +0800

    msp20200923

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
-
+# 定义poll命名空间 detail2 因为有很多服务都可能有detail同名的view方法
+# 所以在这加一个app_name 命名空间
+app_name = 'polls'
 urlpatterns=[
     path('hello/wrold', views.helloWorld, name="hello"),
-    path('index' ,views.index, name="index")
+    # path('' ,views.index, name="index"),
+    path('index/<int:num>' ,views.index2, name="index2"),
+    path('<int:question_id>', views.detail, name="detail"),
+    path('index2/<int:question_id>', views.detail2, name="detail2")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 # the view is a controller
+from .models import Question
+from django.template import loader
+from django.shortcuts import render,get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+# 和上面对比不使用原始的 HttpResponse template.loader.get_template("页面").render(paramMap,request)这种形式
+def index2(request,num):
+    lastquestion = Question.objects.order_by("-pub_date")[:5] # 通过年进行排序
+    contextList = {"latest_question_list":lastquestion}
+    return render(request,'polls/index.html',contextList)
+
+# shortcuts里面封装了一些方便的工具方法 如render替换 template = loader.get_template && template.render('',request)
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def detail2(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def helloWorld(request):
     return HttpResponse("python study");
